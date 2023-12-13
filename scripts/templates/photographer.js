c751fc50772547c53d336c0892d9af566bdd14b3
luc perrot <luc.perrot@hotmail.fr>
2023-12-13 12:27:29 +0100

    fetch data

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,34 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { name, id, city, country, tagline, price, portrait } = data;
+  const picture = `assets/photographers/${portrait}`;
 
-    const picture = `assets/photographers/${portrait}`;
+  function getUserCardDOM() {
+      const article = document.createElement('article');
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+      const img = document.createElement('img');
+      img.setAttribute("src", picture);
+      img.setAttribute("alt", `Portrait de ${name}`);
+
+      const h2 = document.createElement('h2');
+      h2.textContent = name;
+
+      const taglineElement = document.createElement('p');
+      taglineElement.textContent = tagline;
+
+      const locationElement = document.createElement('p');
+      locationElement.textContent = `${city}, ${country}`;
+
+      const priceElement = document.createElement('p');
+      priceElement.textContent = `${price}€/jour`;
+
+      article.appendChild(img);
+      article.appendChild(h2);
+      article.appendChild(taglineElement);
+      article.appendChild(locationElement);
+      article.appendChild(priceElement);
+
+      return article;
+  }
+
+  return { getUserCardDOM };
 }
