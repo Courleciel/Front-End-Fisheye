6271988d3c18821903b263d885b08162654dc56f
luc perrot <luc.perrot@hotmail.fr>
2023-12-22 15:55:54 +0100

    Style on photographer page

---
+++ scripts/factories/MediaFactory.js
@@ -0,0 +1,47 @@
+// MediaFactory.js
+
+function createImageElement(media, photographerName) {
+  const img = document.createElement('img');
+  img.classList.add('photographer-photos');
+  img.src = `assets/FishEye_Photos/Sample Photos/${photographerName}/${media.image}`;
+  img.alt = media.title;
+  return img;
+}
+
+function createVideoElement(media, photographerName) {
+  const video = document.createElement('video');
+  video.classList.add('photographer-videos');
+  video.src = `assets/FishEye_Photos/Sample Photos/${photographerName}/${media.video}`;
+  video.controls = true;
+  return video;
+}
+
+function createMediaElement(media, photographerName, isVideo) {
+  const mediaElement = isVideo ? createVideoElement(media, photographerName) : createImageElement(media, photographerName);
+  const container = document.createElement('div');
+  const infoContainer = document.createElement('div');
+  const title = document.createElement('p');
+  const likes = document.createElement('p');
+  const likeIcon = document.createElement('i');
+
+  container.classList.add('media-container');
+  infoContainer.classList.add('media-informations');
+  title.classList.add('media-title');
+  likes.classList.add('media-likes');
+  likeIcon.classList.add('fa-solid', 'fa-heart');
+
+  title.textContent = media.title;
+  likes.textContent = `${media.likes} `;
+  likes.appendChild(likeIcon);
+
+  infoContainer.appendChild(title);
+  infoContainer.appendChild(likes);
+  container.appendChild(mediaElement);
+  container.appendChild(infoContainer);
+
+  return container;
+}
+
+export default function mediaFactory(media, photographerName) {
+  return createMediaElement(media, photographerName, 'video' in media);
+}
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,13 +1,15 @@
 import photographerTemplate from "../templates/photographer.js";
+import mediaFactory from "../factories/MediaFactory.js";
+
 async function fetchData() {
   try {
-      const response = await fetch('/data/photographers.json');
-      if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return await response.json();
+    const response = await fetch('/data/photographers.json');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
   } catch (error) {
-      console.error('Could not fetch data:', error);
+    console.error('Could not fetch data:', error);
   }
 }
 
@@ -20,15 +22,28 @@ function displayPhotographer(photographer) {
   const photographerSection = document.querySelector(".photograph-header");
   const elements = photographerTemplate(photographer).getUserHeader();
 
-  // Itérer sur chaque élément du tableau et les ajouter au DOM
   elements.forEach(element => {
     photographerSection.appendChild(element);
   });
 }
 
-function displayMedia(media) {
-  // Ici, vous pouvez créer et ajouter des éléments HTML pour afficher les médias du photographe
-  console.log(media);
+function displayMedia(mediaArray, photographerName) {
+  const mediaContainer = document.querySelector("#mediaContainer");
+
+  mediaArray.forEach(media => {
+    const mediaElement = mediaFactory(media, photographerName);
+    mediaContainer.appendChild(mediaElement);
+  });
+}
+
+function displayPhotographerInfo(photographer, mediaArray) {
+  const totalLikes = mediaArray.reduce((acc, media) => acc + media.likes, 0);
+
+  const infoContainer = document.getElementById('photographer-info-container');
+  infoContainer.innerHTML = `
+    <p>${totalLikes} <i class="fa-solid fa-heart"></i></p>
+    <p>${photographer.price}€ / jour</p>
+  `;
 }
 
 async function init() {
@@ -36,16 +51,15 @@ async function init() {
   const photographerId = getPhotographerId();
 
   if (data) {
-      const photographer = data.photographers.find(p => p.id == photographerId);
+    const photographer = data.photographers.find(p => p.id == photographerId);
+    if (photographer) {
+      displayPhotographer(photographer);
       const media = data.media.filter(m => m.photographerId == photographerId);
-
-      if (photographer) {
-          displayPhotographer(photographer);
-      }
-
       if (media.length > 0) {
-          displayMedia(media);
+        displayMedia(media, photographer.name);
+        displayPhotographerInfo(photographer, media);
       }
+    }
   }
 }
 
